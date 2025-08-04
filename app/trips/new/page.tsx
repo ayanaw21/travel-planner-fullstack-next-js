0581a67d26d4d732d553937f74a122d4497485d5
Ayanaw Mengesha <ayanawmengesha21@gmail.com>
2025-08-04 11:36:06 -0700

    feat: create action and component to create new trip and add to prisma

---
+++ app/trips/new/page.tsx
@@ -0,0 +1,105 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { CreateTrips } from "@/lib/actions/create-trips";
+import { cn } from "@/lib/utils";
+import { useTransition } from "react";
+
+export default function NewTrip() {
+    const [isPending,startTransition] = useTransition()
+
+	return (
+		<div className="max-w-lg mx-auto mt-10">
+			<Card>
+				<CardHeader>New Trip</CardHeader>
+				<CardContent>
+					<form action={(formData:FormData)=>{
+                        startTransition(()=>{
+                            CreateTrips(formData)
+                        })
+                    }} className="space-y-6">
+						<div>
+							<label
+								htmlFor="title"
+								className="block text-sm font-medium text-gray-700 mb-1"
+							>
+								Title
+							</label>
+							<input
+								type="text"
+								id="title"
+								name="title"
+								required
+								placeholder="Japan trip..."
+								className={cn(
+									"w-full border border-gray-300 px-3 py-2",
+									"rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+								)}
+							/>
+						</div>
+						<div>
+							<label
+								htmlFor="description"
+								className="block text-sm font-medium text-gray-700 mb-1"
+							>
+								Description
+							</label>
+							<textarea
+								id="description"
+								name="description"
+								placeholder="Trip Description..."
+								required
+								className={cn(
+									"w-full border border-gray-300 px-3 py-2",
+									"rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+								)}
+							/>
+						</div>
+						<div className="grid grid-cols-2 gap-4">
+							<div>
+								<label
+									htmlFor="startDate"
+									className="block text-sm font-medium text-gray-700 mb-1"
+								>
+									Start Date
+								</label>
+								<input
+									type="date"
+									id="startDate"
+									name="startDate"
+									
+									className={cn(
+										"w-full border border-gray-300 px-3 py-2",
+										"rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+									)}
+								/>
+							</div>
+							<div>
+								<label
+									htmlFor="endDate"
+									className="block text-sm font-medium text-gray-700 mb-1"
+								>
+									End Date
+								</label>
+								<input
+									type="date"
+									id="endDate"
+									name="endDate"
+								
+									className={cn(
+										"w-full border border-gray-300 px-3 py-2",
+										"rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+									)}
+								/>
+							</div>
+						</div>
+                        <Button type="submit" disabled={isPending} className="w-full ">
+                            {isPending ? "Creating" : "Create Trip"}
+                        </Button>
+					</form>
+				</CardContent>
+			</Card>
+		</div>
+	);
+}
