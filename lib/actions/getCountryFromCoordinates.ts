7000b40b52764516d35d140c11d130db05a79adc
Ayanaw Mengesha <ayanawmengesha21@gmail.com>
2025-08-06 03:50:22 -0700

    fix type error

--- lib/actions/getCountryFromCoordinates.ts
+++ lib/actions/getCountryFromCoordinates.ts
@@ -1,25 +1,37 @@
-interface geoCodeResult{
-    country:string,
-    formattedAddress:string
+interface GeoCodeResult {
+  country: string;
+  formattedAddress: string;
 }
-export const getCountryFromCoordinates = async(lat:number,lng:number):Promise<geoCodeResult>=>{
-    const apiKey = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
-	const response = await fetch(
-		`https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?access_token=${apiKey}`
-	);
-    const data = await response.json();
 
-    if (!data.features || data.features.length === 0) {
+interface Feature {
+  place_type: string[];
+  text: string;
+  place_name: string;
+}
+
+export const getCountryFromCoordinates = async (
+  lat: number,
+  lng: number
+): Promise<GeoCodeResult> => {
+  const apiKey = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN;
+
+  const response = await fetch(
+    `https://api.mapbox.com/geocoding/v5/mapbox.places/${lng},${lat}.json?access_token=${apiKey}`
+  );
+
+  const data = await response.json();
+
+  if (!data.features || data.features.length === 0) {
     throw new Error("No results found.");
   }
-  const result =  data.features[0]
-  const countryFeature = data.features.find((feature: any) =>
-    feature.place_type.includes("country")
+
+  const result: Feature = data.features[0];
+  const countryFeature: Feature | undefined = data.features.find(
+    (feature: Feature) => feature.place_type.includes("country")
   );
 
   return {
-    country:countryFeature.text || "unKnown",
-    formattedAddress:result.place_name || "Unknown address"
-  }
-    
-}
+    country: countryFeature?.text || "Unknown",
+    formattedAddress: result.place_name || "Unknown address",
+  };
+};
